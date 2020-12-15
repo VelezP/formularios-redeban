879428a678eb4bfa8a1f05e41b2d82cd4bd75c17
Diego Herrera <daherrerac@gmail.com>
2020-12-15 09:26:48 -0500

    Cambios
    Cambios

--- fomulario-solicitud-equipo/js/forms.js
+++ fomulario-solicitud-equipo/js/forms.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                 required: true,
                 email: true,
                 minlength: 4,
-                maxlength: 25
+                maxlength: 35
                 },
                 ismerchant: {
                     required:function(){
